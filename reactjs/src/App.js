e52b9a1d4150532fc56d0b0fce2f58bc64b4517b
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-05-22 07:54:58 +0530

    added styles to the login page

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,54 +1,69 @@
-import logo from './Linkedin-Logo.webp'
-import './App.css';
+import logo from "./Linkedin-Logo.webp";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <img src={logo} id="linkedin-logo" alt="LinkedIn" />
-      <div class="Container">
-      <section class="login-form">
-        <form id="login" action="">
-          <table class="tables">
-            <tr>
-              <td id="tbl-heading">
-                <h1>Sign in</h1>
-                <p id="para-font">Stay updated on your professional world</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input
-                  type="text"
-                  class="username"
-                  name="username"
-                  id="text-box"
-                  placeholder="Email or Phone"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input
-                  type="password"
-                  class="password"
-                  name="password"
-                  id="text-box"
-                  placeholder="Password"
-                />
-              </td>
-            </tr>
-            <tr>
-                <td id="forgot-pwd"><a href="#">Forgot password?</a></td>
-            </tr>
-            <tr>
-              <td id="tdata">
-                <button class="btn" type="button">Submit</button>
-              </td>
-            </tr>
-          </table>
-        </form>
-      </section>
-     </div>       
+    <>      
+      <div className="Container">
+        <img src={logo} id="linkedin-logo" alt="LinkedIn" />
+        <section className="login-form">
+          <form id="login" action="">
+            <table className="tables">
+              <tr>
+                <td id="tbl-heading">
+                  <h1>Sign in</h1>
+                  <p id="para-font">Stay updated on your professional world</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    className="username"
+                    name="username"
+                    id="text-box"
+                    placeholder="Email or Phone"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    type="password"
+                    className="password"
+                    name="password"
+                    id="text-box"
+                    placeholder="Password"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td id="forgot-pwd">
+                  <a href="#">Forgot password?</a>
+                </td>
+              </tr>
+              <tr>
+                <td id="tdata">
+                  <button className="btn" type="button">
+                    Submit
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p id="sign-up">
+                    New to LinkedIn <a href="#">Join Now</a>
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </form>
+        </section>
+      </div>
+
+      <footer className="container">
+        <img src={logo} id="footer-logo" alt="LinkedIn" />
+      </footer>
     </>
   );
 }
